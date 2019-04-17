5ec5793aee4cca6164bdf1085d9a6e843eb97f1c
Patryk Węgrzyn <wegpat@gmail.com>
2019-04-18 00:33:22 +0200

    Not an elegenat way to handle this

---
+++ src/main/java/pl/edu/agh/io/jappka/os/UnsupportedOsException.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.io.jappka.os;
+
+public class UnsupportedOsException extends Exception {
+
+    public String toString() {
+        return "An unsupported OS has been detected!";
+    }
+
+}
