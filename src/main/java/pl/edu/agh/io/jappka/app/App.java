d3be70bb99c058a17ddd2e84c16d0c556b0ff2b7
WoLewicki <lewywojt@gmail.com>
2019-04-22 14:57:58 +0200

    Stage application moved to APPGUI class

--- src/main/java/pl/edu/agh/io/jappka/app/App.java
+++ src/main/java/pl/edu/agh/io/jappka/app/App.java
@@ -1,21 +1,14 @@
 package pl.edu.agh.io.jappka.app;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import pl.edu.agh.io.jappka.activity.*;
 import org.apache.commons.exec.OS;
-import pl.edu.agh.io.jappka.controller.AppController;
-import pl.edu.agh.io.jappka.os.WindowsNativeAccessor;
-import pl.edu.agh.io.jappka.util.Utils;
-
-import java.io.IOException;
+import pl.edu.agh.io.jappka.presenter.AppGUI;
+;
 
 
 public class App extends Application {
-    private Stage primaryStage;
 
     public static void main(String[] args) {
         launch(args);
@@ -38,20 +31,8 @@ public class App extends Application {
         chromeSummary.generate();
         chromeSummary.getAllPeriods().forEach(period -> System.out.println(period.generateInfo()));
 
-        this.primaryStage = primaryStage;
-        initApplication();
-    }
-
-    private void initApplication() throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getClassLoader().getResource("Jappka/view/mainView.fxml"));
-        BorderPane rootLayout = loader.load();
-
-        Scene scene = new Scene(rootLayout);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        AppController controller = loader.getController();
-        controller.setPrimaryStageElements(primaryStage, scene);
+        AppGUI gui = new AppGUI(primaryStage);
+        gui.initApplication();
     }
 
     public static void quit() {
--- src/main/java/pl/edu/agh/io/jappka/presenter/AppGUI.java
+++ src/main/java/pl/edu/agh/io/jappka/presenter/AppGUI.java
@@ -1,9 +1,29 @@
 package pl.edu.agh.io.jappka.presenter;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import pl.edu.agh.io.jappka.controller.AppController;
+
+import java.io.IOException;
 
 public class AppGUI {
-    public AppGUI(Stage mainStage){
+    private Stage primaryStage;
 
+    public AppGUI(Stage primaryStage){
+        this.primaryStage = primaryStage;
+    }
+
+    public void initApplication() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("Jappka/view/mainView.fxml"));
+        BorderPane rootLayout = loader.load();
+
+        Scene scene = new Scene(rootLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        AppController controller = loader.getController();
+        controller.setPrimaryStageElements(primaryStage, scene);
     }
 }
