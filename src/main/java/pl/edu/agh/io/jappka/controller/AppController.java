ff5ac4d1188703de9f693850eab2cc63d2d86a97
Latitude/Timelock <tomekgsd@vp.pl>
2019-04-24 11:35:34 +0200

    Gant chart added and basic config + gitignore change and .idea removal

--- src/main/java/pl/edu/agh/io/jappka/controller/AppController.java
+++ src/main/java/pl/edu/agh/io/jappka/controller/AppController.java
@@ -1,20 +1,24 @@
 package pl.edu.agh.io.jappka.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import pl.edu.agh.io.jappka.activity.AbstractActivityPeriod;
+import pl.edu.agh.io.jappka.charts.GanttChart;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Observable;
 
 public class AppController {
 
@@ -23,13 +27,16 @@ public class AppController {
     private Scene graphScene;
     private ObservableMap<String, List<AbstractActivityPeriod>> obData;
 
+
+    private GanttChart<Number,String> mainChart;
+
     @FXML
-    private StackedBarChart mainChart;
+    private AnchorPane mainPane;
 
     public void setPrimaryStageElements(Stage primaryStage, Scene primaryScene) {
         this.primaryStage = primaryStage;
         this.primaryScene = primaryScene;
-        prepareBarChart();
+        initGanttChart();
     }
 
     public void setObData(ObservableMap<String, List<AbstractActivityPeriod>> obData){
@@ -89,14 +96,41 @@ public class AppController {
         }
     }
 
-    private void prepareBarChart() {
+    private void initGanttChart() {
+        NumberAxis xAxis = new NumberAxis();
+        CategoryAxis yAxis = new CategoryAxis();
+        mainChart = new GanttChart<Number, String>(xAxis,yAxis);
+        configureAxis(xAxis,yAxis);
+        configureChart();
+        //Add chart to the main pane
+        ObservableList list = mainPane.getChildren();
+        list.addAll(mainChart);
+
+    }
+
+    private void configureChart(){
+        mainChart.setTitle("Title TBD");
+        mainChart.setLegendVisible(true);
+        mainChart.setBlockHeight(50);
         mainChart.getData().clear();
         mainChart.setCursor(Cursor.CROSSHAIR);
-        mainChart.setTitle("Usage state");
+        mainChart.setTitle("Usage State");
         mainChart.setAnimated(false);
-        mainChart.getXAxis().setTickLabelRotation(90);
-        mainChart.getXAxis().setLabel("Time");
-        mainChart.getYAxis().setLabel("Applications");
+        mainChart.setLayoutY(22.0);
+        mainChart.setPrefHeight(405.0);
+        mainChart.setPrefWidth(700.0);
+    }
+
+    private void configureAxis(NumberAxis xAxis, CategoryAxis yAxis){
+        xAxis.setLabel("Time");
+        xAxis.setTickLabelFill(Color.CHOCOLATE);
+        xAxis.setMinorTickCount(4);
+        yAxis.setLabel("Applications");
+        yAxis.setTickLabelFill(Color.CHOCOLATE);
+        yAxis.setTickLabelGap(10);
+        //get initial category list
+        String[] categories = obData.keySet().toArray(new String[0]);
+        yAxis.setCategories(FXCollections.<String>observableArrayList(Arrays.asList(categories)));
     }
 
     public ObservableMap<String, List<AbstractActivityPeriod>> getObData() {
